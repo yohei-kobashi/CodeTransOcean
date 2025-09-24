2f820494b18df7eb50136f7e50416c61397cf559
yohei-kobashi <yohei.kobashi@gmail.com>
2025-09-24 23:33:56 +0900

    misc

--- CodeLLM/convert_score_json_to_csv.py
+++ CodeLLM/convert_score_json_to_csv.py
@@ -6,9 +6,11 @@ output = []
 for json_file in glob.glob("scores/*.json"):
     data = json.load(open(json_file))
     for score_name in ["blue", "codeblue", "DSR"]:
-        output.append([json_file, "overall", score_name, data["overall"]["DSR"]])
+        if not score_name in data["overall"]:
+            continue
+        output.append([json_file, "overall", score_name, data["overall"][score_name]])
         for pair,v in data["per_pair"].items():
-            output.append([json_file, pair, score_name, data["per_pair"][pair]["DSR"]])
+            output.append([json_file, pair, score_name, data["per_pair"][pair][score_name]])
 
 csv.writer(open("scores/all_scores.tsv"), delimiter="\t").writerows(output)
             
